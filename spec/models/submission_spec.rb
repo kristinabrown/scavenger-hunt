499145171d50637de1c90ab3ff02bca770679014
Kristina Brown <kristina.frey.frey@gmail.com>
2015-07-28 17:40:12 -0600

    testing paperclip/aws works

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
+require 'rack/test'
 
 RSpec.describe Submission, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "is valid" do
+    it "is valid with valid attributes" do
+      submission = Submission.new
+      expect(submission).to be_valid
+    end
+    #this works but I don't want to keep running it...
+    xit "can accept a photo" do
+      submission = Submission.create
+      submission.attachment = Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/sample.jpg', 'image/jpg')
+      expect(submission.attachment.url).to eq("http://scavengerhunter.s3.amazonaws.com/submissions/attachments/000/000/002/original/sample.jpg?1438126732")
+    end
+  end
 end
