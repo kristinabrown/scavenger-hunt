3964897befb935210b8a1cb39dc2188b3b262fa9
Kristina Brown <kristina.frey.frey@gmail.com>
2015-07-28 16:38:54 -0600

    submissions model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150728223714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "submissions", force: :cascade do |t|
+    t.boolean  "correct"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
 end
