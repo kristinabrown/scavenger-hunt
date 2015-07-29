3f64aa82bc803bb916e4c02220991d4d8d95bbb9
end3rs-army <eric.m.dowty@gmail.com>
2015-07-29 09:30:25 -0600

    added relationships between teams and hunts

--- spec/models/hunt_spec.rb
+++ spec/models/hunt_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe :Hunt, type: :model do
 
-  let!(:hunt) { create(:hunt) }
+  let!(:hunt)  { create(:hunt) }
+  let(:team)   { create(:team) }
+  let(:team_2) { create(:team_2) }
+  let(:team_3) { create(:team_3) }
 
   it 'is valid with proper attributes' do
     expect(hunt).to be_valid
@@ -25,6 +28,17 @@ RSpec.describe :Hunt, type: :model do
     expect(hunt).to be_invalid
     hunt.name = 'TO'
     expect(hunt).to be_valid
+  end
+
+  it 'can have a team assigned' do
+    hunt.teams << team
+    expect(hunt.teams.count).to eq(1)
+  end
+
+  it 'can have multiple teams assigned' do
+    teams = [team, team_2, team_3]
+    teams.each { |t| hunt.teams << t }
+    expect(hunt.teams.count).to eq(3)
   end
 
 end
