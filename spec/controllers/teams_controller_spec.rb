5f65b5b4aa95f4ff098a7888800f11feef08a782
Andrew Carmer <carmerandrew@gmail.com>
2015-07-29 22:46:50 -0600

    remove faker from gemfile. team controller update and index

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe TeamsController, type: :controller do
   let!(:submission_3) { create(:submission_3)}
   let!(:submission_4) { create(:submission_4)}
 
+  before(:each) do
+    team1.hunt_id = hunt.id
+    team2.hunt_id = hunt.id
+  end
+
   describe "POST #create" do
     it "creates a new team" do
       beginning_count = Team.count
@@ -27,8 +32,31 @@ RSpec.describe TeamsController, type: :controller do
   end
 
   describe "PATCH #update" do
-    it "updates the team name" do
+    xit "update the team name" do
+      expect(team1.name).to eq("team 1")
 
+      params = {name: "New Name", id: team1.id}
+      patch :update, format: :json, team: params
+
+      expect(team1.name).to eq("New Name")
+    end
+  end
+
+  describe "GET #index" do
+    it "gets an index of all teams on current hunt" do
+      params = {phone_number: "12223334440" , hunt_id: hunt.id}
+      post :create, format: :json, team: params
+
+      params = {phone_number: "12223334449" , hunt_id: hunt.id}
+      post :create, format: :json, team: params
+
+
+      params = {hunt_id: hunt.id}
+
+      get :index, format: :json, team: params
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data.count).to eq(2)
     end
   end
 
