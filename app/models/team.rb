9942e62948840e8ccd08ee6ccee749ddbef1488d
Andrew Carmer <carmerandrew@gmail.com>
2015-08-03 13:45:25 -0600

    materialize and logo

--- app/models/team.rb
+++ app/models/team.rb
@@ -30,8 +30,8 @@ class Team < ActiveRecord::Base
     unique_start = @location_ids.sample
     while already_taken.include?(unique_start)
       unique_start = @location_ids.sample
-    end 
-    self.update(location_id: unique_start)
+    end
+    self.location_id = unique_start
   end
 
   def self.on_current_hunt(hunt_id)
