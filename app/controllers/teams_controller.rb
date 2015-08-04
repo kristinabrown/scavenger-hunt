e18596d056ded78125271878537a5e516e4453eb
end3rs-army <eric.m.dowty@gmail.com>
2015-08-03 22:01:23 -0600

    added API endpoint for team data

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -20,15 +20,14 @@ class TeamsController < ApplicationController
     @team = Team.find_by(slug: params[:id])
   end
 
+  def team_data
+    respond_with Team.find_by!(id: params[:id]).data, location: nil
+  end
 
   private
 
   def team_params
-    params.require(:team).permit(:hunt_id,
-                                 :location_id,
-                                 :name,
-                                 :phone_number,
-                                 :found_locations)
+    params.require(:team).permit(:hunt_id, :location_id, :name, :phone_number, :found_locations, :hunt_initiated)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: "admin#dashboard"
 
-
   resources :submissions, only: [:create, :index, :update]
   get '/latest-submission', to: 'submissions#latest_submission'
+
   resources :teams, only: [:create, :update, :show, :index]
+  get '/team_data/:id', to: 'teams#team_data'
+
   resources :hunts, only: [:create, :destroy, :index]
 end
