177c54b2103ff8cb84de73c56994ca5ebf5986c0
end3rs-army <eric.m.dowty@gmail.com>
2015-08-07 16:28:26 -0600

    added localhost functionality for Redis

--- app/assets/javascripts/redis_client.js
+++ app/assets/javascripts/redis_client.js
@@ -1,11 +1,9 @@
 function connectToRedis(){
-  console.log("connectToRedis() fired")
   var socket = io.connect('http://localhost:3001/');
 
   socket.on('message', function(){
     setView();
     gatherCurrentHuntData();
-    console.log("WE SHOULD HAVE RE-RENDERED")
   });
 
 };
